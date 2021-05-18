8ce23fee72d8e4f95632ad1ef7e5057fd5fe807d
krsailesh <krsailesh2001@gmail.com>
2021-05-18 17:17:26 +0530

    comment added and client_uri made public

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
-const { CLIENT_ID, CLIENT_SECRET, CLIENT_URI, REFRESH_TOKEN } = require("./config")
+const { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN } = require("./config")
 const OAuth2 = google.auth.OAuth2;
+const CLIENT_URI = "https://developers.google.com/oauthplayground";
+const oauth2client = new OAuth2(CLIENT_ID, CLIENT_SECRET, CLIENT_URI);      // Creating the oauth client object.
 
-const oauth2client = new OAuth2(CLIENT_ID, CLIENT_SECRET, CLIENT_URI);
-
-oauth2client.setCredentials({
+oauth2client.setCredentials({               // Setting up refresh token, that will help to get new access token.
     refresh_token: REFRESH_TOKEN
 });
 
 async function sendMail() {
     try {
-        const accessToken = await oauth2client.getAccessToken();
-        const smtpTransporter = nodemailer.createTransport({
+        const accessToken = await oauth2client.getAccessToken();        // Getting the new access token which changes every 5000 secong.
+        const smtpTransporter = nodemailer.createTransport({            // Creating the SMTP transproter through nodemailer.
             service: "gmail",
             auth: {
                 type: "OAuth2",
-                user: "krsailesh2001@gmail.com",
+                user: "krsailesh2001@gmail.com",                        // Client.
                 clientId: CLIENT_ID,
                 clientSecret: CLIENT_SECRET,
                 refreshToken: REFRESH_TOKEN,
@@ -24,21 +24,21 @@ async function sendMail() {
             }
         });
 
-        const mailOptions = {
+        const mailOptions = {                                           // Mail related information.
             from: "krsailesh2001@gmail.com",
             to: "krsailesh2001@gmail.com",
             subject: "Emailing through google api",
             text: "successful testing"
         };
 
-        const result = await smtpTransporter.sendMail(mailOptions);
+        const result = await smtpTransporter.sendMail(mailOptions);     // Sending the mail which returns promise
         return result;
 
-    } catch (error) {
+    } catch (error) {               
         return error
     }
 }
 
 sendMail()
-    .then(res => console.log("Email sent...", res))
-    .catch(err => console.log(err));
+    .then(res => console.log("Email sent...", res))         // Logging the response after successful email sent.
+    .catch(err => console.log(err));                        // Logging the error on unseccessful email set.
